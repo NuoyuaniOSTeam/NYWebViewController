baf637f4988bf8a3790d8c446d1008cab17f2e9d
NYJZZ <appleco@nuoyuan.com.cn>
2018-06-12 16:08:37 +0800

    update.

--- NYWebViewController/Example/NYViewController.m
+++ NYWebViewController/Example/NYViewController.m
@@ -73,7 +73,22 @@
 }
 
 - (void)webViewController:(NYWebViewController *)webViewController didReceiveScriptMessage:(NYScriptMessage *)message {
-    
+    NSDictionary *dict = message.params;
+    NSString *textToShare = @"分享的标题。";
+    NSURL *urlToShare = [NSURL URLWithString:[dict objectForKey:@"url"]];
+    NSArray *activityItems = @[textToShare, urlToShare];
+    UIActivityViewController *activityVC = [[UIActivityViewController alloc]initWithActivityItems:activityItems applicationActivities:nil];
+    activityVC.excludedActivityTypes = @[UIActivityTypePrint, UIActivityTypeCopyToPasteboard,UIActivityTypeAssignToContact,UIActivityTypeSaveToCameraRoll];
+    [self presentViewController:activityVC animated:YES completion:nil];
+    activityVC.completionWithItemsHandler = ^(UIActivityType  _Nullable activityType, BOOL completed, NSArray * _Nullable returnedItems, NSError * _Nullable activityError) {
+        if (completed) {
+            NSLog(@"completed");
+            //分享 成功
+        } else  {
+            NSLog(@"cancled");
+            //分享 取消
+        }
+    };
 }
 
 - (void)addScriptMessageHandler {
